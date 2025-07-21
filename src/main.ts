c4bdd992ba643c668cf5e1506ea7b3d0acdcb3ac
Francisco Damião Terto <contato@damiaoterto.dev>
2025-07-21 15:42:58 -0300

    chore: enable versioning

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableVersioning({ type: VersioningType.URI });
 
   await app.listen(process.env.PORT ?? 3000);
 }
